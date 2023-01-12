11172e290f0a53082ab31ad8987a9ab88ff3f929
rickyjoe11 <syrus.samson99@gmail.com>
2023-01-12 16:32:31 -0500

    Added functionality

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ console.clear();
 
 
 //get text box
-textBox = document.getElementById
+textBox = document.getElementById('textBox')
+
 //get plate 
 
-//function for text overlay
-if ((document.getElementById('plateClassic')).checked) {
 
-}
+//get the checkbox
+classic = $('#plateClassic')
+
+//function for text overlay
+
+textBox.addEventListener('keyup', e => {
+    console.log(textBox.value)
+
+    //change image of plate on left side
+
+    //get the text overlay element
+    text = $('#plateText').text('').text(textBox.value);
+
+}) 
