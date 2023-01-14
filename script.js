7077a38b2d51c37a37073a66bbd7eda64e38d092
rickyjoe11 <syrus.samson99@gmail.com>
2023-01-14 11:26:38 -0500

    added new pages

--- script.js
+++ script.js
@@ -15,24 +15,28 @@ plate1.addEventListener('mouseup', e => {
     $('#plateImg').attr('src', '').attr('src', 'imgs/plate1.png')
     //add custom text styling
     $('#plateText').css('color', '#00407e');
+    $('#plateTitle').text('').text('Ontario (white)')
 })
 plate2.addEventListener('mouseup', e => {
     //change plate
     $('#plateImg').attr('src', '').attr('src', 'imgs/plate2.png')
     //add custom text styling
     $('#plateText').css('color', 'white');
+    $('#plateTitle').text('').text('Ontario (blue)')
 })
 plate3.addEventListener('mouseup', e => {
     //change plate
     $('#plateImg').attr('src', '').attr('src', 'imgs/plate3.png')
     //add custom text styling
     $('#plateText').css('color', 'rgb(43, 57, 71)');
+    $('#plateTitle').text('').text('Quebec')
 })
 plate4.addEventListener('mouseup', e => {
     //change plate
     $('#plateImg').attr('src', '').attr('src', 'imgs/plate4.png')
     //add custom text styling
     $('#plateText').css('color', 'rgb(170, 16, 16)');
+    $('#plateTitle').text('').text('Alberta')
 })
 
 
